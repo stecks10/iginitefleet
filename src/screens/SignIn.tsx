3751a2d95fcbafe45295bf07fe881eb28ea43f35
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-19 09:22:25 -0300

    Interface login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Container, RNText } from './styles';
+import { Container, Slogan, Title } from './styles';
+
+import backgroundImg from '../assets/background.png';
 
 export function Signin() {
   return (
-    <Container>
-      <RNText>Signin</RNText>
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
     </Container>
   );
 }
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -1,13 +1,25 @@
 import styled from 'styled-components/native';
+import theme from '../theme';
 
-export const Container = styled.View`
+export const Container = styled.ImageBackground`
   flex: 1;
-  align-items: center;
   justify-content: center;
-  background-color: #202024;
+  padding: 52px;
+  background-color: ${theme.COLORS.GRAY_800};
 `;
 
-export const RNText = styled.Text`
-  color: #fff;
-  font-size: 20px;
+export const Title = styled.Text`
+  font-family: ${theme.FONT_FAMILY.BOLD};
+  font-size: ${theme.FONT_SIZE.XXXL};
+  color: ${theme.COLORS.BRAND_LIGHT};
+  text-align: center;
+`;
+
+export const Slogan = styled.Text`
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  font-size: ${theme.FONT_SIZE.MD};
+  color: ${theme.COLORS.GRAY_100};
+  text-align: center;
+
+  margin-bottom: 32px;
 `;
