c63aaa8cd68cb41f8082a7df25de7544e3a6613a
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-18 22:15:49 -0300

    Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { Loading } from './src/components/Loading';
 import { Signin } from './src/screens/SignIn';
 import theme from './src/theme';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Signin />;
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Signin />
     </ThemeProvider>
   );
 }
