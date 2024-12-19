66c990e0fe2e6f41954d5763789516dff1471c78
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-18 21:29:26 -0300

    Criando projeto

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function Signin() {
+  return (
+    <View style={styles.container}>
+      <Text>Signin</Text>
+    </View>
+  );
+}
