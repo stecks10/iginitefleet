519da7997a9f71589c14736490e96f5adf237989
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-19 10:27:02 -0300

    ambiente

--- App.tsx
+++ App.tsx
@@ -4,11 +4,11 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto';
 
+import { ANDROID_CLIENT_ID } from '@env';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from './src/components/Loading';
 import { Signin } from './src/screens/SignIn';
-
 import theme from './src/theme';
 
 export default function App() {
@@ -16,6 +16,7 @@ export default function App() {
     Roboto_400Regular,
     Roboto_700Bold,
   });
+  console.log(ANDROID_CLIENT_ID);
 
   if (!fontsLoaded) {
     return <Loading />;
