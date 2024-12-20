cd9879038775d88aa7539a2b39f2cb18718d6f69
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-19 21:36:47 -0300

    Login com Google

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto';
 
-import { ANDROID_CLIENT_ID } from '@env';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from './src/components/Loading';
@@ -16,7 +15,6 @@ export default function App() {
     Roboto_400Regular,
     Roboto_700Bold,
   });
-  console.log(ANDROID_CLIENT_ID);
 
   if (!fontsLoaded) {
     return <Loading />;
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
-import { Container, Slogan, Title } from './styles';
-
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import React, { useState } from 'react';
 import backgroundImg from '../assets/background.png';
 import { Button } from '../components/Button';
+import { Container, Slogan, Title } from './styles';
+
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env';
+import { Alert } from 'react-native';
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
 
 export function Signin() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+
+      const { idToken } = await GoogleSignin.signIn();
+
+      if (!idToken) {
+      } else {
+        Alert.alert('Não foi possível realizar o login');
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível realizar o login');
+      setIsAuthenticating(false);
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title='Entrar com Google' />
+      <Button
+        title='Entrar com Google'
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   );
 }
