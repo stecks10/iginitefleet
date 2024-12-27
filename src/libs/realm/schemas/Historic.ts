1ea6be523a9a59e8e98564d9edd9ab83402ebd51
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-27 16:57:44 -0300

    Método generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,7 +1,30 @@
 import { Realm } from '@realm/react';
 
+type GenerateProps = {
+  user_id: string;
+  description: string;
+  license_plate: string;
+};
+
 export class Historic extends Realm.Object<Historic> {
-  static generate() {}
+  _id!: string;
+  user_id!: string;
+  license_plate!: string;
+  description!: string;
+  status!: string;
+  created_at!: Date;
+  updated_at!: Date;
+  static generate({ user_id, description, license_plate }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      license_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+  }
 
   static schema = {
     name: 'Historic',
