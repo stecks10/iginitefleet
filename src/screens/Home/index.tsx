e4c668688ee947f3561dc47f49a9e22ab0441906
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-01-07 11:21:20 -0300

    Exibindo dados do banco

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
+import { useEffect } from 'react';
 import { CarStatus } from '../../components/CarStatus';
 import { HomeHeader } from '../../components/HomeHeader';
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
 import { Container, Content } from './styles';
 
 export function Home() {
   const { navigate } = useNavigation();
+
+  const historic = useQuery(Historic);
+
+  function fetchVehicle() {
+    console.log(historic);
+  }
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
 
   function handleRegisterMovement() {
     navigate('departure');
