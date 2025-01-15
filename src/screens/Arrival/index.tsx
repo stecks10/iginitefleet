55b5939631fabd523d5a253d9116008402f61676
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-01-15 09:47:31 -0300

    Cancelar o veículo em uso

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,12 +1,11 @@
-import { useRoute } from '@react-navigation/native';
-import { Button } from '../../components/Button';
-import { ButtonIcon } from '../../components/ButtonIcon';
-import { Header } from '../../components/Header';
-
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { X } from 'phosphor-react-native';
+import { Alert } from 'react-native';
+
 import { BSON } from 'realm';
-import { useObject } from '../../libs/realm';
+import { useObject, useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
+
 import {
   Container,
   Content,
@@ -16,29 +15,52 @@ import {
   LicensePlate,
 } from './styles';
 
-type RouteParamsProps = {
+import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { Header } from '../../components/Header';
+
+type RouteParamProps = {
   id: string;
 };
 
 export function Arrival() {
   const route = useRoute();
-  const { id } = route.params as RouteParamsProps;
+  const { id } = route.params as RouteParamProps;
 
+  const realm = useRealm();
+  const { goBack } = useNavigation();
   const historic = useObject(Historic, new BSON.UUID(id));
+
+  function handleRemoveVehicleUsage() {
+    Alert.alert('Cancelar', 'Cancelar a utilização do veículo?', [
+      { text: 'Não', style: 'cancel' },
+      { text: 'Sim', onPress: () => removeVehicleUsage() },
+    ]);
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() => {
+      realm.delete(historic);
+    });
+
+    goBack();
+  }
 
   return (
     <Container>
       <Header title='Chegada' />
       <Content>
-        <Label>Placa do Veiculo</Label>
+        <Label>Placa do veículo</Label>
 
-        <LicensePlate>{(historic as Historic)?.license_plate}</LicensePlate>
+        <LicensePlate>{historic?.license_plate}</LicensePlate>
+
         <Label>Finalidade</Label>
 
-        <Description>{(historic as Historic)?.description}</Description>
+        <Description>{historic?.description}</Description>
 
         <Footer>
-          <ButtonIcon icon={X} />
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
+
           <Button title='Registrar chegada' />
         </Footer>
       </Content>
