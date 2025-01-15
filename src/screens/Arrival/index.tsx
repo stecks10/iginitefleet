7b6029a63831d787d42a70b600e74f20c1baa642
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-01-15 08:40:22 -0300

    Criando a tela para registrar chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,14 @@
 import { useRoute } from '@react-navigation/native';
-import { Container } from './styles';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from './styles';
 
 type RouteParamsProps = {
   id: string;
@@ -8,5 +17,26 @@ type RouteParamsProps = {
 export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header title='Chegada' />
+      <Content>
+        <Label>Placa do Veiculo</Label>
+
+        <LicensePlate>{id}</LicensePlate>
+        <Label>Finalidade</Label>
+
+        <Description>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum vel
+          laborum corporis. Nobis, adipisci officiis voluptas, eius blanditiis
+          magni sequi, iure quos eaque exercitationem alias fugiat. Quas illum
+          facere dolore!
+        </Description>
+
+        <Footer>
+          <Button title='Registrar chegada' />
+        </Footer>
+      </Content>
+    </Container>
+  );
 }
--- src/screens/Arrival/styles.ts
+++ src/screens/Arrival/styles.ts
@@ -1,5 +1,42 @@
 import styled from 'styled-components/native';
+import theme from '../../theme';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${theme.COLORS.GRAY_800};
+`;
+
+export const Content = styled.View`
+  flex-grow: 1;
+  padding: 32px;
+`;
+
+export const Label = styled.Text`
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  font-size: ${theme.FONT_SIZE.SM}px;
+  color: ${theme.COLORS.GRAY_300};
+
+  margin-top: 32px;
+  margin-bottom: 5px;
+`;
+
+export const LicensePlate = styled.Text`
+  font-family: ${theme.FONT_FAMILY.BOLD};
+  font-size: ${theme.FONT_SIZE.XXXL}px;
+  color: ${theme.COLORS.GRAY_100};
+`;
+
+export const Description = styled.Text`
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  font-size: ${theme.FONT_SIZE.MD}px;
+  color: ${theme.COLORS.GRAY_100};
+
+  text-align: justify;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  gap: 16px;
+  margin-top: 32px;
 `;
